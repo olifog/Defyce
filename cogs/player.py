03b056c3b5643e784c283fc4808a855241ed18f3
Oli Fog <oliver@fogelin.com>
2020-07-12 23:10:58 +0100

    format change

--- cogs/player.py
+++ cogs/player.py
@@ -182,6 +182,7 @@ class player(commands.Cog):
 
             user = pdata['displayname']
 
+        hyplayer = await self.bot.hypixelapi.getPlayer(name=user)
         xplist = None
 
         defy = await self.bot.db.defy.find_one({})
@@ -208,12 +209,12 @@ class player(commands.Cog):
             desc += day + "\t\t**" + str(xplist[day]) + "**\n"
 
         embed = discord.Embed(timestamp=datetime.now(tz=self.bot.est), description=desc, title=title)
-        embed.set_thumbnail(url=self.bot.user.avatar_url_as(format="png"))
-        embed.set_footer(text="Defy Guild")
-
+        embed.set_thumbnail(url="https://visage.surgeplay.com/full/" + hyplayer.UUID)
+        embed.set_footer(text="Defy Guild",
+                         icon_url="https://cdn.discordapp.com/attachments/677996730741948431/678268510400544790/Screenshot_2020-02-14_at_19.36.43.png")
 
         embed.set_author(name=user + "\'s XP",
-                            icon_url="https://cdn.discordapp.com/attachments/677996730741948431/678268510400544790/Screenshot_2020-02-14_at_19.36.43.png")
+                            icon_url=self.bot.user.avatar_url_as(format="png"))
         await ctx.send(embed=embed)
 
 

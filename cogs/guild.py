b5acdf37aab3597e489d6bccb1f8575f355703d0
Oli Fog <oliver@fogelin.com>
2020-09-24 19:00:39 +0100

    smaller reqs

--- cogs/guild.py
+++ cogs/guild.py
@@ -159,7 +159,7 @@ class guild(commands.Cog):
 
         await channel.send(dispguildname + " Guild rank checks")
 
-        for x in range((len(results)//18)+1):
+        for x in range((len(results)//15)+1):
             embed = discord.Embed(description='\n'.join(results[x*16:(x+1)*16]))
             await channel.send(embed=embed)
 
